1af5291637b318467ac6a4136a64350d352212f8
David Rios <david.rios.valencia.al@iespoligonosur.org>
2025-10-21 19:56:43 +0200

    Si

--- Tienda/views.py
+++ Tienda/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from .models import Usuario,PerfilUsuario
-# Create your views here.
+from .models import Usuario,PerfilUsuario,Marca,Descuento,Prenda,Inventario,Pedido,DetallePedido,Reseña,Cesta
+from django.db.models import Q
+
+
+def index(request):
+    return render(request, 'Tienda/index.html')
 
 def lista_PerfilUsuario(request):
     perfiles=PerfilUsuario.objects.select_related("usuario").all()
     #perfiles=(PerfilUsuario.objects.raw("SELECT * FROM Tienda_perfilusuario p"
     #                                 +" JOIN Tienda_usuario u ON u.id = p.usuario_id"))
     return render(request,'Tienda/lista_perfil_usuario.html',{'lista_PerfilUsuario': perfiles})
-
+    
