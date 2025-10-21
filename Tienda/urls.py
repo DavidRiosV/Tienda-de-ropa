303628ba588467d824e1fa0e28155811af8d8ed4
David <david.rios.valencia.al@iespoligonosur.org>
2025-10-21 08:29:05 +0200

    Index

--- Tienda/urls.py
+++ Tienda/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
+        path('',views.index,name='index.html'),
         path('perfil_usuario/lista',views.lista_PerfilUsuario, name='lista_PerfilUsuario'),
-]       
+]               
--- Tienda/views.py
+++ Tienda/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Usuario,PerfilUsuario
 # Create your views here.
+def index(request):
+    return render(request, 'Tienda/index.html')
 
 def lista_PerfilUsuario(request):
     perfiles=PerfilUsuario.objects.select_related("usuario").all()
