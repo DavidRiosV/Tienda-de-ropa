8578e79308979a648d59efdfddf71fd3acfa9f00
David <david.rios.valencia.al@iespoligonosur.org>
2025-10-21 10:34:35 +0200

    URL_Terminada_Con_Raw

--- Tienda/views.py
+++ Tienda/views.py
@@ -13,4 +13,10 @@ def lista_PerfilUsuario(request):
 def lista_Cesta(request):
     cesta=Cesta.objects.select_related("usuario").prefetch_related("prenda")
     cesta = cesta.order_by("fecha_creacion")
-    return render(request,'Tienda/lista_cesta.html',{'lista_Cesta': cesta})
+    #cesta = Cesta.objects.raw("SELECT* FROM Tienda_cesta c "
+    #   " JOIN Tienda_usuario u ON u.id = c.usuario_id"
+    #   " JOIN Tienda_cesta_prenda cp ON cp.cesta_id = c.id"
+    #   " JOIN Tienda_prenda p ON p.id = cp.prenda_id"
+    #   " ORDER BY c.fecha_creacion"
+    #)
+    return render(request, 'Tienda/lista_cesta.html', {'lista_Cesta': cesta})
