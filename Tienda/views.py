3ba44b5bd9a3be536da68c2337dda946783aba4c
David Rios <david.rios.valencia.al@iespoligonosur.org>
2025-10-21 19:57:50 +0200

    Arreglos

--- Tienda/views.py
+++ Tienda/views.py
@@ -11,12 +11,11 @@ def lista_PerfilUsuario(request):
     return render(request,'Tienda/lista_perfil_usuario.html',{'lista_PerfilUsuario': perfiles})
 
 def lista_Cesta(request):
-    cesta=Cesta.objects.select_related("usuario").prefetch_related("prenda")
-    cesta = cesta.order_by("fecha_creacion")
-    #cesta = Cesta.objects.raw("SELECT* FROM Tienda_cesta c "
+    cestas = Cesta.objects.select_related("usuario").prefetch_related("prenda").order_by("fecha_creacion")
+    #cestas = Cesta.objects.raw("SELECT* FROM Tienda_cesta c "
     #   " JOIN Tienda_usuario u ON u.id = c.usuario_id"
     #   " JOIN Tienda_cesta_prenda cp ON cp.cesta_id = c.id"
     #   " JOIN Tienda_prenda p ON p.id = cp.prenda_id"
     #   " ORDER BY c.fecha_creacion"
     #)
-    return render(request, 'Tienda/lista_cesta.html', {'lista_Cesta': cesta})
+    return render(request, 'Tienda/lista_cesta.html', {'lista_Cesta': cestas})
