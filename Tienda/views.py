cfd1d86ca2707b9976a9bb623ef9d67941be59e3
David Rios <david.rios.valencia.al@iespoligonosur.org>
2025-10-22 19:00:08 +0200

    Termine

--- Tienda/views.py
+++ Tienda/views.py
@@ -50,4 +50,13 @@ def lista_prendas(request):
 
 def lista_prendas(request):
     prendas = Prenda.objects.select_related('marca', 'descuento').prefetch_related('usuarios').filter(usuarios__isnull=True).order_by('nombre')
+    '''
+    prendas = Prenda.objects.raw("SELECT * FROM Tienda_prenda p"
+                                 " JOIN Tienda_marca m ON m.id =p.marca_id"
+                                 " JOIN Tienda_descuento d ON d.id =p.descuento_id"
+                                 " LEFT JOIN Tienda_prenda_usuarios pu ON pu.prenda_id = p.id"
+                                 " WHERE pu.id IS NULL"
+                                 " ORDER BY p.nombre"
+    )
+    '''
     return render(request, 'Tienda/lista_prendas.html', {'lista_prendas': prendas})
