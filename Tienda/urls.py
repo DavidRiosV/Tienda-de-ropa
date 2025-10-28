290140d3b6c0ea78e893a2383494558ba8cfc579
David <david.rios.valencia.al@iespoligonosur.org>
2025-10-28 11:17:40 +0100

    Cambio11

--- Tienda/urls.py
+++ Tienda/urls.py
@@ -8,12 +8,8 @@ urlpatterns = [
         path('usuario/<int:id_usuario>/', views.dame_usuario, name='dame_usuario'),
         path('descuento/<int:porcentaje>/', views.lista_descuentos, name='lista_descuentos'),
         path('marcas/<str:palabra>/<str:pais>', views.lista_marcas, name='lista_marcas'),
-<<<<<<< HEAD
-        path("__debug__/", include("debug_toolbar.urls")),
-=======
         path('prenda/lista',views.lista_prendas, name='lista_prendas'),
         path('reseña/lista',views.lista_reseñas, name='lista_reseñas'),
         path('pedido/<int:id_pedido>/', views.lista_pedido_prendas, name='lista_pedido_prendas'),
         path('detalles_pedido/lista', views.lista_detallepedido_total, name='lista_detallepedido_total')
->>>>>>> Tarea_URL_URL9
 ]
