145550eddc70078cb9700dc6341ee3e50c4fa328
David Rios <david.rios.valencia.al@iespoligonosur.org>
2025-11-10 19:22:46 +0100

    Nada

--- Tienda/urls.py
+++ Tienda/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from.import views
 
 urlpatterns = [
-        path('',views.index,name='index.html'),
+        path('',views.index,name='index'),
         path('perfil_usuario/lista',views.lista_PerfilUsuario, name='lista_PerfilUsuario'),
         path('cesta/lista',views.lista_Cesta, name='lista_Cesta'),
         path('usuario/<int:id_usuario>/', views.dame_usuario, name='dame_usuario'),
