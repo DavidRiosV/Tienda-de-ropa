43b0039d7d882b9070b52f8fc69d39e68435e3d6
David <david.rios.valencia.al@iespoligonosur.org>
2025-11-11 08:37:22 +0100

    Debug

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,13 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Tienda.urls')),
-
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
+
+
 
 handler400 = 'Tienda.views.mi_error_400'
 handler403 = 'Tienda.views.mi_error_403'
