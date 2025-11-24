397ad70baf861d7c48323e23ccbdd52c90ab8f2e
David Rios <david.rios.valencia.al@iespoligonosur.org>
2025-11-24 19:38:10 +0100

    siii

--- Tienda/urls.py
+++ Tienda/urls.py
@@ -21,7 +21,4 @@ urlpatterns = [
 
         #---Marca---
         path('marca/crear',views.marca_create, name='marca_create'),
-
-        #---Descuento---
-        path('descuento/crear',views.descuento_create, name='descuento_create'),
 ]
